ca6adf4ddc284f67617ffb62ddcc95052d244f9b
Zogot <leon@rowland.nl>
2013-02-05 20:21:26 +0100

    Init method

--- lib/Zogo/Punchin/Plugin.php
+++ lib/Zogo/Punchin/Plugin.php
@@ -2,4 +2,12 @@
 
 class Zogo_Punchin_Plugin {
     
+    public function __construct() {
+        
+        add_action( 'init', array( $this, 'init' ) );
+    }
+    
+    public function init() {
+        
+    }
 }
