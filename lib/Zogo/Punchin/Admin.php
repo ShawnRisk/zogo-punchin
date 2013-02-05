dc25e39f3e9e9044af4f4e3c04c08c2b4559c4c2
Zogot <leon@rowland.nl>
2013-02-05 20:27:52 +0100

    Starting meta box method

--- lib/Zogo/Punchin/Admin.php
+++ lib/Zogo/Punchin/Admin.php
@@ -2,4 +2,12 @@
 
 class Zogo_Punchin_Admin {
     
+    public function __construct() {
+        
+        add_action( 'add_meta_boxes', array( $this, 'meta_boxes' ) );
+    }
+    
+    public function meta_boxes() {
+        
+    }
 }
