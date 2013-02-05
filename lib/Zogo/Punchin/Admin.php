877ccc7422f16d0096cb0a77e57301dd35b262fc
Zogot <leon@rowland.nl>
2013-02-05 20:29:55 +0100

    Metabox and starting associated methods

--- lib/Zogo/Punchin/Admin.php
+++ lib/Zogo/Punchin/Admin.php
@@ -9,5 +9,21 @@ class Zogo_Punchin_Admin {
     
     public function meta_boxes() {
         
+        add_meta_box( 
+            'zogo_punchin_main_metabox', 
+            __( 'Timecard Details', 'zogo-punchin-domain' ), 
+            array( $this, 'view_main_metabox' ), 
+            'zogo_punchin', 
+            'advanced', 
+            'high' 
+        );
+    }
+    
+    public function view_main_metabox() {
+        
+    }
+    
+    public function save_main_metabox() {
+        
     }
 }
