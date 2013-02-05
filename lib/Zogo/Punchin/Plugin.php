974feda63175dbb0a9addd2609ffcd5fc8ee8815
Zogot <leon@rowland.nl>
2013-02-05 20:26:46 +0100

    Custom Post type

--- lib/Zogo/Punchin/Plugin.php
+++ lib/Zogo/Punchin/Plugin.php
@@ -9,5 +9,35 @@ class Zogo_Punchin_Plugin {
     
     public function init() {
         
+        register_post_type( 'zogo_punchin',
+            array(
+                'labels' => array(
+                    'name'                  => _x( 'Timecards', 'plural name post type', 'zogo-punchin-domain' ),
+                    'singular_name'         => _x( 'Timecard', 'singular name post type', 'zogo-punchin-domain' ),
+                    'add_new'               => _x( 'Add New', 'punchin', 'zogo-punchin-domain' ),
+                    'add_new_item'          => __( 'Add New Timecard', 'zogo-punchin-domain' ),
+                    'edit_item'             => __( 'Edit Timecard', 'zogo-punchin-domain' ),
+                    'new_item'              => __( 'New Timecard', 'zogo-punchin-domain' ),
+                    'all_items'             => __( 'All Timecards', 'zogo-punchin-domain' ),
+                    'view_item'             => __( 'View Timecard', 'zogo-punchin-domain' ),
+                    'search_items'          => __( 'Search Timecards', 'zogo-punchin-domain' ),
+                    'not_found'             => __( 'No timecards found', 'zogo-punchin-domain' ),
+                    'not_found_in_trash'    => __( 'No timecards found in Trash', 'zogo-punchin-domain' ),
+                    'parent_item_colon'     => '',
+                    'menu_name'             => __( 'Timecards', 'zogo-punchin-domain' )
+                ),
+                'public'                        => true,
+                'publicly_queryable'            => true,
+                'show_ui'                       => true,
+                'show_in_menu'                  => true,
+                'query_var'                     => true,
+                'rewrite'                       => array( 'slug' => __( 'timecard', 'zogo-punchin-domain' ) ),
+                'capability_type'               => 'post',
+                'has_archive'                   => true,
+                'hierarchical'                  => false,
+                'menu_position'                 => null,
+                'supports'                      => array( '' )
+            )
+        );
     }
 }
