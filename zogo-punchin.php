7911f86e73f53284739a611fdb93800a2b098c6f
Zogot <leon@rowland.nl>
2013-02-05 20:18:56 +0100

    Starting plugin file.

--- zogo-punchin.php
+++ zogo-punchin.php
@@ -7,3 +7,41 @@
  * Version 0.0.1
  */
 
+/////////////////
+// DEFINITIONS //
+/////////////////
+
+define( 'ZOGO_PUNCHIN_ROOT', __DIR__ );
+define( 'ZOGO_PUNCHIN_FOLDER', basename( ZOGO_PUNCHIN_ROOT ) );
+define( 'ZOGO_PUNCHIN_URL', plugins_url( '', __FILE__ ) );
+
+
+////////////////////////
+// PRONAMIC FRAMEWORK //
+////////////////////////
+
+if ( ! class_exists( 'Pronamic_Base_Autoload' ) )
+    include ZOGO_PUNCHIN_ROOT . '/pronamic-framework/Pronamic/Base/Autoload.php';
+
+$autoload = Pronamic_Base_Autoload::get_instance();
+
+$autoload->register_components( [
+    "Pronamic\\Base" => ZOGO_PUNCHIN_ROOT . '/pronamic-framework',
+    "Pronamic\\Helper" => ZOGO_PUNCHIN_ROOT . '/pronamic-framework'
+] );
+
+$autoload->register_folders( [
+    ZOGO_PUNCHIN_ROOT . '/lib'
+] );
+
+$autoload->register();
+
+
+//////////////////
+// START PLUGIN //
+//////////////////
+
+$plugin = new Zogo_Punchin_Plugin();
+
+if ( is_admin() )
+    $admin = new Zogo_Punchin_Admin();
